3b7acc87b7573d8a0809e7c72fa58f2d54bb3a72
Zoe Goldschmidt <zoe.gld@gmail.com>
2021-01-24 14:46:19 +0000

    Exercise 5.1

---
+++ part5/bloglist-frontend/src/App.js
@@ -0,0 +1,81 @@
+import React, { useState, useEffect } from 'react'
+import Blog from './components/Blog'
+import blogService from './services/blogs'
+import loginService from './services/login'
+
+const App = () => {
+  const [blogs, setBlogs] = useState([])
+  const [user, setUser] = useState(null)
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+  useEffect(() => {
+    blogService.getAll().then(blogs =>
+      setBlogs( blogs )
+    )
+  }, [])
+
+
+  const loginForm = () => (
+    <>
+      <h1>Login to application</h1>
+      <form onSubmit={handleLogin}>
+        <div>
+          username
+            <input
+            type="text"
+            value={username}
+            name="Username"
+            onChange={({ target }) => setUsername(target.value)}
+          />
+        </div>
+        <div>
+          password
+            <input
+            type="password"
+            value={password}
+            name="Password"
+            onChange={({ target }) => setPassword(target.value)}
+          />
+        </div>
+        <button type="submit">login</button>
+      </form>
+    </>
+  )
+
+  const blogsList = () => (
+    <>
+      <h2>blogs</h2>
+      <p> {user.name} is logged in.</p>
+      {blogs.map(blog =>
+        <Blog key={blog.id} blog={blog} />
+      )}
+    </>
+  )
+
+  const handleLogin = async (event) => {
+    event.preventDefault()
+    console.log('logging in with', username, password)
+    try {
+      const user = await loginService.login({
+        username, password,
+      })
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch (exception) {
+      console.log(exception)
+    }
+  }
+
+  return (
+    <div>
+      {user
+        ? blogsList()
+        : loginForm()
+      }
+    </div>
+  )
+}
+
+export default App
